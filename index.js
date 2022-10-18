a2d4ce8b9fec43a930ff58ed485e04f511ed60c6
Alton Fo <altonfo109@gmail.com>
2022-10-18 14:59:35 -0400

    commit tree e8ccf60d8c2edf229f67dd76bf21d3c9e511d25d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const dgm = ["Allen", "Lenalee", "Kanda"];
+
+const end = []
+function writeCards(dgm, event) {
+    for (let x=0; x<dgm.length; x++) {
+        end.push(`Thank you, ${dgm[x]}, for the wonderful ${event} gift!`);
+    }
+    return end;
+}
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--)
+    }
+    
+}
